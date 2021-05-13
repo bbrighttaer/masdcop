b4e706cc212bd7bb133ca82913b838b6f5ec482f
bbrighttaer <brighteragyemang@gmail.com>
2021-05-13 04:27:28 +0000

    project setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='masdcop',
+    version='0.0.0',
+    packages=['proj', 'masdcop'],
+    url='https://github.com/bbrighttaer/masdcop',
+    license='MIT',
+    author='bbrighttaer',
+    author_email='ba233@uowmail.edu.au',
+    description='Implementations of DCOP algorithms'
+)
